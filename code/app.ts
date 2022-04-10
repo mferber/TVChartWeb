8a598dc2961558941c2cff5d06cfe868b7ad71ee
Matthias Ferber <matthias.ferber@gmail.com>
2022-04-09 21:30:59 -0400

    Made API a little more RESTful + improved editor
    * Replaced POST to /edit with PUT to /data and
      added a placeholder for PATCH
    * Replaced form-based editing with an API-friendly
      scripted submission

--- code/app.ts
+++ code/app.ts
@@ -6,19 +6,28 @@ const app = express()
 const port = 8000
 const dataFilePath = '../data/shows.csv';
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.text({type: 'text/plain'}));
+
 app.use(express.static('frontend/dist'));
 
+// GET main data
 app.get('/data', async (req: Request, res: Response): Promise<void> => {
   res.set('Content-Type', 'text/plain')
   res.send(await fs.promises.readFile(dataFilePath, 'utf-8'));
 });
 
-app.post('/edit', async (req: Request, res: Response): Promise<void> => {
-  saveEditedDataFile(req.body.text, dataFilePath);
-  res.redirect('/');
+// PUT main data -- replaces old version
+app.put('/data', async (req: Request, res: Response): Promise<void> => {
+  await saveEditedDataFile(req.body, dataFilePath);
+  res.status(200).send();
 });
 
+// PATCH main data -- modify most recently watched episode for a show
+app.patch('/data', async(req: Request, res: Response): Promise<void> => {
+
+});
+
+// Start server
 app.listen(port, () => {
   console.log(`TV app listening on port ${port}`)
 });
