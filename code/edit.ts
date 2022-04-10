890466a87e3b0cebed711a8261c9c8f43eb2603e
Matthias Ferber <matthias.ferber@gmail.com>
2022-04-09 21:40:45 -0400

    Safer save when replacing data file w/submission

--- code/edit.ts
+++ code/edit.ts
@@ -1,5 +1,7 @@
 import fs from 'fs';
 
 export async function saveEditedDataFile(text: string, dataFilePath: string): Promise<void> {
-  return fs.promises.writeFile(dataFilePath, text, 'utf-8');
+  const tempFilePath = dataFilePath + '.tmp'
+  await fs.promises.writeFile(tempFilePath, text, 'utf-8');
+  await fs.promises.rename(tempFilePath, dataFilePath);
 }
